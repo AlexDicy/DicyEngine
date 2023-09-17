7031bb075c53ea2b77444d194416f08efaef1198
Lorenzo Lapucci <halexercraft@gmail.com>
2023-09-17 15:49:11 +0200

    Rewrite the EventDispatcher to support multiple events type with one dispatcher instance

--- engine/src/events/dispatcher.rs
+++ engine/src/events/dispatcher.rs
@@ -1,23 +1,39 @@
+use std::any::{Any, TypeId};
+use std::collections::HashMap;
+use std::sync::{Mutex, MutexGuard};
+use lazy_static::lazy_static;
 use crate::events::event::Event;
 
-pub struct EventDispatcher<T: Event> {
-    handlers: Vec<fn(&T)>,
+pub struct EventDispatcher {
+    handlers: HashMap<TypeId, Vec<Box<dyn Any + Send + Sync>>>,
 }
 
-impl<T: Event> EventDispatcher<T> {
-    pub fn new() -> EventDispatcher<T> {
+
+impl EventDispatcher {
+    fn new() -> EventDispatcher {
         EventDispatcher {
-            handlers: Vec::new(),
+            handlers: HashMap::new(),
         }
     }
 
-    pub fn register_handler(&mut self, handler: fn(&T)) {
-        self.handlers.push(handler);
+    pub fn register_handler<T: 'static + Event>(&mut self, handler: fn(&T)) {
+        self.handlers.entry(TypeId::of::<T>()).or_insert(Vec::new()).push(Box::new(handler));
     }
 
-    pub fn dispatch(&self, event: &T) {
-        for handler in &self.handlers {
-            handler(event);
+    pub fn dispatch<T: 'static + Event>(&self, event: &T) {
+        if let Some(handlers) = self.handlers.get(&TypeId::of::<T>()) {
+            for handler in handlers {
+                let handler = handler.downcast_ref::<fn(&T)>().unwrap();
+                handler(event);
+            }
         }
     }
+}
+
+lazy_static! {
+    pub static ref DISPATCHER: Mutex<EventDispatcher> = Mutex::new(EventDispatcher::new());
+}
+
+pub fn get_event_dispatcher() -> MutexGuard<'static, EventDispatcher> {
+    DISPATCHER.lock().unwrap()
 }
