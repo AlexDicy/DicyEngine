834afa0b6ca2939eaa8f23188b370168839bfa34
Lorenzo Lapucci <halexercraft@gmail.com>
2023-09-25 14:13:46 +0200

    Group dependencies as nested projects and set "Sandbox" as the startup project

--- premake5.lua
+++ premake5.lua
@@ -7,6 +7,8 @@ workspace "DicyEngine"
         "Dist"
     }
 
+    startproject "Sandbox"
+
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
 
@@ -119,198 +121,201 @@ project "Sandbox"
         optimize "on"
 
 
-project "GLFW"
-    location "Engine/libs/GLFW"
-    kind "StaticLib"
-    language "C"
-    warnings "off"
+group "Dependencies"
+    project "GLFW"
+        location "Engine/libs/GLFW"
+        kind "StaticLib"
+        language "C"
+        warnings "off"
 
-    targetdir ("Engine/libs/GLFW/bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("Engine/libs/GLFW/bin/int/" .. outputdir .. "/%{prj.name}")
-
-    files {
-        "Engine/libs/GLFW/include/GLFW/glfw3.h",
-        "Engine/libs/GLFW/include/GLFW/glfw3native.h",
-        "Engine/libs/GLFW/src/glfw_config.h",
-        "Engine/libs/GLFW/src/context.c",
-        "Engine/libs/GLFW/src/init.c",
-        "Engine/libs/GLFW/src/input.c",
-        "Engine/libs/GLFW/src/monitor.c",
-        
-        "Engine/libs/GLFW/src/null_init.c",
-        "Engine/libs/GLFW/src/null_joystick.c",
-        "Engine/libs/GLFW/src/null_monitor.c",
-        "Engine/libs/GLFW/src/null_window.c",
-        
-        "Engine/libs/GLFW/src/platform.c",
-        "Engine/libs/GLFW/src/vulkan.c",
-        "Engine/libs/GLFW/src/window.c"
-    }
-
-    filter "system:windows"
-        staticruntime "off"
-
-        files { 
-            "Engine/libs/GLFW/src/win32_init.c",
-            "Engine/libs/GLFW/src/win32_joystick.c",
-            "Engine/libs/GLFW/src/win32_module.c",
-            "Engine/libs/GLFW/src/win32_monitor.c",
-            "Engine/libs/GLFW/src/win32_time.c",
-            "Engine/libs/GLFW/src/win32_thread.c",
-            "Engine/libs/GLFW/src/win32_window.c",
-            "Engine/libs/GLFW/src/wgl_context.c",
-            "Engine/libs/GLFW/src/egl_context.c",
-            "Engine/libs/GLFW/src/osmesa_context.c"
-        }
-
-        defines { 
-            "_GLFW_WIN32",
-            "_CRT_SECURE_NO_WARNINGS"
-        }
-
-    filter "system:linux"
-        pic "on"
-   
-        systemversion "latest"
+        targetdir ("Engine/libs/GLFW/bin/" .. outputdir .. "/%{prj.name}")
+        objdir ("Engine/libs/GLFW/bin/int/" .. outputdir .. "/%{prj.name}")
 
         files {
-            "Engine/libs/GLFW/src/src/x11_init.c",
-            "Engine/libs/GLFW/src/src/x11_monitor.c",
-            "Engine/libs/GLFW/src/src/x11_window.c",
-            "Engine/libs/GLFW/src/src/xkb_unicode.c",
-            "Engine/libs/GLFW/src/src/posix_module.c",
-            "Engine/libs/GLFW/src/src/posix_time.c",
-            "Engine/libs/GLFW/src/src/posix_thread.c",
-            "Engine/libs/GLFW/src/src/posix_module.c",
-            "Engine/libs/GLFW/src/src/glx_context.c",
-            "Engine/libs/GLFW/src/src/egl_context.c",
-            "Engine/libs/GLFW/src/src/osmesa_context.c",
-            "Engine/libs/GLFW/src/src/linux_joystick.c"
-        }
-   
-        defines {
-            "_GLFW_X11"
+            "Engine/libs/GLFW/include/GLFW/glfw3.h",
+            "Engine/libs/GLFW/include/GLFW/glfw3native.h",
+            "Engine/libs/GLFW/src/glfw_config.h",
+            "Engine/libs/GLFW/src/context.c",
+            "Engine/libs/GLFW/src/init.c",
+            "Engine/libs/GLFW/src/input.c",
+            "Engine/libs/GLFW/src/monitor.c",
+            
+            "Engine/libs/GLFW/src/null_init.c",
+            "Engine/libs/GLFW/src/null_joystick.c",
+            "Engine/libs/GLFW/src/null_monitor.c",
+            "Engine/libs/GLFW/src/null_window.c",
+            
+            "Engine/libs/GLFW/src/platform.c",
+            "Engine/libs/GLFW/src/vulkan.c",
+            "Engine/libs/GLFW/src/window.c"
         }
 
-    filter "system:macosx"
-        pic "on"
-    
+        filter "system:windows"
+            staticruntime "off"
+
+            files { 
+                "Engine/libs/GLFW/src/win32_init.c",
+                "Engine/libs/GLFW/src/win32_joystick.c",
+                "Engine/libs/GLFW/src/win32_module.c",
+                "Engine/libs/GLFW/src/win32_monitor.c",
+                "Engine/libs/GLFW/src/win32_time.c",
+                "Engine/libs/GLFW/src/win32_thread.c",
+                "Engine/libs/GLFW/src/win32_window.c",
+                "Engine/libs/GLFW/src/wgl_context.c",
+                "Engine/libs/GLFW/src/egl_context.c",
+                "Engine/libs/GLFW/src/osmesa_context.c"
+            }
+
+            defines { 
+                "_GLFW_WIN32",
+                "_CRT_SECURE_NO_WARNINGS"
+            }
+
+        filter "system:linux"
+            pic "on"
+       
+            systemversion "latest"
+
+            files {
+                "Engine/libs/GLFW/src/src/x11_init.c",
+                "Engine/libs/GLFW/src/src/x11_monitor.c",
+                "Engine/libs/GLFW/src/src/x11_window.c",
+                "Engine/libs/GLFW/src/src/xkb_unicode.c",
+                "Engine/libs/GLFW/src/src/posix_module.c",
+                "Engine/libs/GLFW/src/src/posix_time.c",
+                "Engine/libs/GLFW/src/src/posix_thread.c",
+                "Engine/libs/GLFW/src/src/posix_module.c",
+                "Engine/libs/GLFW/src/src/glx_context.c",
+                "Engine/libs/GLFW/src/src/egl_context.c",
+                "Engine/libs/GLFW/src/src/osmesa_context.c",
+                "Engine/libs/GLFW/src/src/linux_joystick.c"
+            }
+       
+            defines {
+                "_GLFW_X11"
+            }
+
+        filter "system:macosx"
+            pic "on"
+        
+            files {
+                "Engine/libs/GLFW/src/cocoa_init.m",
+                "Engine/libs/GLFW/src/cocoa_monitor.m",
+                "Engine/libs/GLFW/src/cocoa_window.m",
+                "Engine/libs/GLFW/src/cocoa_joystick.m",
+                "Engine/libs/GLFW/src/cocoa_time.c",
+                "Engine/libs/GLFW/src/nsgl_context.m",
+                "Engine/libs/GLFW/src/posix_thread.c",
+                "Engine/libs/GLFW/src/posix_module.c",
+                "Engine/libs/GLFW/src/osmesa_context.c",
+                "Engine/libs/GLFW/src/egl_context.c"
+            }
+
+            defines {
+                "_GLFW_COCOA"
+            }
+
+        filter "configurations:Debug"
+            runtime "Debug"
+            symbols "on"
+        
+        filter { "system:windows", "configurations:Debug-AS" }    
+            runtime "Debug"
+            symbols "on"
+            sanitize { "Address" }
+            flags { "NoRuntimeChecks", "NoIncrementalLink" }
+        
+        filter "configurations:Release"
+            runtime "Release"
+            optimize "speed"
+        
+        filter "configurations:Dist"
+            runtime "Release"
+            optimize "speed"
+            symbols "off"
+
+
+    project "glad"
+        location "Engine/libs/glad"
+        kind "StaticLib"
+        language "C"
+        warnings "off"
+
+        targetdir ("Engine/libs/glad/bin/" .. outputdir .. "/%{prj.name}")
+        objdir ("Engine/libs/glad/bin/int/" .. outputdir .. "/%{prj.name}")
+
         files {
-            "Engine/libs/GLFW/src/cocoa_init.m",
-            "Engine/libs/GLFW/src/cocoa_monitor.m",
-            "Engine/libs/GLFW/src/cocoa_window.m",
-            "Engine/libs/GLFW/src/cocoa_joystick.m",
-            "Engine/libs/GLFW/src/cocoa_time.c",
-            "Engine/libs/GLFW/src/nsgl_context.m",
-            "Engine/libs/GLFW/src/posix_thread.c",
-            "Engine/libs/GLFW/src/posix_module.c",
-            "Engine/libs/GLFW/src/osmesa_context.c",
-            "Engine/libs/GLFW/src/egl_context.c"
+            "Engine/libs/glad/include/glad/gl.h",
+            "Engine/libs/glad/include/KHR/khrplatform.h",
+            "Engine/libs/glad/src/gl.c"
         }
 
-        defines {
-            "_GLFW_COCOA"
+        includedirs {
+            "Engine/libs/glad/include"
         }
 
-    filter "configurations:Debug"
-        runtime "Debug"
-        symbols "on"
-    
-    filter { "system:windows", "configurations:Debug-AS" }    
-        runtime "Debug"
-        symbols "on"
-        sanitize { "Address" }
-        flags { "NoRuntimeChecks", "NoIncrementalLink" }
-    
-    filter "configurations:Release"
-        runtime "Release"
-        optimize "speed"
-    
-    filter "configurations:Dist"
-        runtime "Release"
-        optimize "speed"
-        symbols "off"
+        filter "system:windows"
+            staticruntime "off"
+            systemversion "latest"
+
+        filter "configurations:Debug"
+            runtime "Debug"
+            symbols "on"
+        
+        filter "configurations:Release"
+            runtime "Release"
+            optimize "speed"
+        
+        filter "configurations:Dist"
+            runtime "Release"
+            optimize "speed"
+            symbols "off"
 
 
-project "glad"
-    location "Engine/libs/glad"
-    kind "StaticLib"
-    language "C"
-    warnings "off"
+    project "ImGui"
+        location "Engine/libs/ImGui"
+        kind "StaticLib"
+        language "C++"
+        warnings "off"
 
-    targetdir ("Engine/libs/glad/bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("Engine/libs/glad/bin/int/" .. outputdir .. "/%{prj.name}")
+        targetdir ("Engine/libs/ImGui/bin/" .. outputdir .. "/%{prj.name}")
+        objdir ("Engine/libs/ImGui/bin/int/" .. outputdir .. "/%{prj.name}")
 
-    files {
-        "Engine/libs/glad/include/glad/gl.h",
-        "Engine/libs/glad/include/KHR/khrplatform.h",
-        "Engine/libs/glad/src/gl.c"
-    }
+        files {
+            "Engine/libs/ImGui/imconfig.h",
+            "Engine/libs/ImGui/imgui.h",
+            "Engine/libs/ImGui/imgui.cpp",
+            "Engine/libs/ImGui/imgui_draw.cpp",
+            "Engine/libs/ImGui/imgui_internal.h",
+            "Engine/libs/ImGui/imgui_tables.cpp",
+            "Engine/libs/ImGui/imgui_widgets.cpp",
+            "Engine/libs/ImGui/imstb_rectpack.h",
+            "Engine/libs/ImGui/imstb_textedit.h",
+            "Engine/libs/ImGui/imstb_truetype.h",
+            "Engine/libs/ImGui/imgui_demo.cpp"
+        }
 
-    includedirs {
-        "Engine/libs/glad/include"
-    }
+        filter "system:windows"
+            staticruntime "off"
+            systemversion "latest"
+            cppdialect "C++17"
 
-    filter "system:windows"
-        staticruntime "off"
-        systemversion "latest"
+        filter "system:linux"
+            pic "On"
+            systemversion "latest"
+            cppdialect "C++17"
 
-    filter "configurations:Debug"
-        runtime "Debug"
-        symbols "on"
-    
-    filter "configurations:Release"
-        runtime "Release"
-        optimize "speed"
-    
-    filter "configurations:Dist"
-        runtime "Release"
-        optimize "speed"
-        symbols "off"
+        filter "configurations:Debug"
+            runtime "Debug"
+            symbols "on"
 
+        filter "configurations:Release"
+            runtime "Release"
+            optimize "on"
 
-project "ImGui"
-    location "Engine/libs/ImGui"
-    kind "StaticLib"
-    language "C++"
-    warnings "off"
+        filter "configurations:Dist"
+            runtime "Release"
+            optimize "on"
+            symbols "off"
 
-    targetdir ("Engine/libs/ImGui/bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("Engine/libs/ImGui/bin/int/" .. outputdir .. "/%{prj.name}")
-
-    files {
-        "Engine/libs/ImGui/imconfig.h",
-        "Engine/libs/ImGui/imgui.h",
-        "Engine/libs/ImGui/imgui.cpp",
-        "Engine/libs/ImGui/imgui_draw.cpp",
-        "Engine/libs/ImGui/imgui_internal.h",
-        "Engine/libs/ImGui/imgui_tables.cpp",
-        "Engine/libs/ImGui/imgui_widgets.cpp",
-        "Engine/libs/ImGui/imstb_rectpack.h",
-        "Engine/libs/ImGui/imstb_textedit.h",
-        "Engine/libs/ImGui/imstb_truetype.h",
-        "Engine/libs/ImGui/imgui_demo.cpp"
-    }
-
-    filter "system:windows"
-        staticruntime "off"
-        systemversion "latest"
-        cppdialect "C++17"
-
-    filter "system:linux"
-        pic "On"
-        systemversion "latest"
-        cppdialect "C++17"
-
-    filter "configurations:Debug"
-        runtime "Debug"
-        symbols "on"
-
-    filter "configurations:Release"
-        runtime "Release"
-        optimize "on"
-
-    filter "configurations:Dist"
-        runtime "Release"
-        optimize "on"
-        symbols "off"
+group ""
