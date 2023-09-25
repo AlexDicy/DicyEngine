df299d5de375e6cba5237953e590a3b3c5fbb4f6
Lorenzo Lapucci <halexercraft@gmail.com>
2023-09-25 21:18:14 +0200

    Hide EventDispatcher implementation details and fix memory issues

--- Engine/src/events/dispatcher.cpp
+++ Engine/src/events/dispatcher.cpp
@@ -1,7 +1,41 @@
 #include "pch/enginepch.h"
 #include "dispatcher.h"
 
-EventDispatcher* EventDispatcher::get() {
+EventDispatcher *EventDispatcher::get() {
     static auto instance = new EventDispatcher();
     return instance;
+}
+
+namespace {
+    using HandlerMap = std::unordered_map<std::type_index, std::vector<void *>>;
+    HandlerMap global_handlers;
+    std::vector<HandlerMap> layer_handlers;
+}
+
+void EventDispatcher::add_global_handler(const std::type_index &type_index, void *handler_ptr) {
+    global_handlers[type_index].push_back(handler_ptr);
+}
+
+void EventDispatcher::add_layer_handler(const unsigned layer_index, const std::type_index &type_index, void *handler_ptr) {
+    if (layer_index >= layer_handlers.size()) {
+        layer_handlers.resize(layer_index + 1);
+    }
+    layer_handlers[layer_index][type_index].push_back(handler_ptr);
+}
+
+
+void EventDispatcher::handle_event(const std::type_index &type_index, const Event &event) {
+    // global handlers
+    for (const auto &handler : global_handlers[type_index]) {
+        const auto handler_ptr = static_cast<Handler<> *>(handler);
+        (*handler_ptr)(event);
+    }
+
+    // layer handlers
+    for (auto &layer_handler : layer_handlers) {
+        for (const auto &handler : layer_handler[type_index]) {
+            const auto handler_ptr = static_cast<Handler<> *>(handler);
+            (*handler_ptr)(event);
+        }
+    }
 }
--- Engine/src/events/dispatcher.h
+++ Engine/src/events/dispatcher.h
@@ -3,48 +3,34 @@
 #include "pch/enginepch.h"
 #include "event.h"
 
+// ReSharper disable CppMemberFunctionMayBeStatic
 class EventDispatcher {
     template <typename E = Event>
     using Handler = std::function<void(const E &)>;
-    using HandlerMap = std::unordered_map<std::type_index, std::vector<void *>>;
-
-    HandlerMap global_handlers;
-    std::vector<HandlerMap> layer_handlers;
 
 public:
     EventDispatcher() = default;
 
     template <typename E = Event>
     void register_global_handler(Handler<E> handler) {
-        auto handler_ptr = new Handler<E>(handler);
-        global_handlers[typeid(E)].push_back(handler_ptr);
+        add_global_handler(typeid(E), new Handler<E>(handler));
     }
 
     template <typename E = Event>
     void register_layer_handler(const unsigned int layer_index, Handler<E> handler) {
-        if (layer_index >= this->layer_handlers.size()) {
-            this->layer_handlers.resize(layer_index + 1);
-        }
-        auto handler_ptr = new Handler<E>(handler);
-        layer_handlers[layer_index][typeid(E)].push_back(handler_ptr);
+        add_layer_handler(layer_index, typeid(E), new Handler<E>(handler));
     }
 
     template <typename E = Event>
     void dispatch(const E &event) {
-        // global handlers
-        for (const auto &handler : this->global_handlers[typeid(E)]) {
-            auto handler_ptr = static_cast<Handler<E> *>(handler);
-            (*handler_ptr)(event);
-        }
-
-        // layer handlers
-        for (auto it = this->layer_handlers.begin(); it != this->layer_handlers.end(); ++it) {
-            for (const auto &handler : (*it)[typeid(E)]) {
-                auto handler_ptr = static_cast<Handler<E> *>(handler);
-                (*handler_ptr)(event);
-            }
-        }
+        handle_event(typeid(E), event);
     }
 
     DE_API static EventDispatcher *get();
+
+private:
+    // hide the implementation details, solves a bunch of memory issues when vectors need to be resized
+    DE_API static void add_global_handler(const std::type_index &type_index, void *handler_ptr);
+    DE_API static void add_layer_handler(unsigned int layer_index, const std::type_index &type_index, void *handler_ptr);
+    DE_API static void handle_event(const std::type_index &type_index, const Event &event);
 };
