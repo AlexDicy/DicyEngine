db23922f7ab91c0c9b51658c20a1706e8defe3cb
Lorenzo Lapucci <halexercraft@gmail.com>
2024-09-22 16:53:13 +0100

    Upload test object

--- Engine/src/scene/models/model_importer.cpp
+++ Engine/src/scene/models/model_importer.cpp
@@ -2,10 +2,12 @@
 #include "model_importer.h"
 
 #include <assimp/Importer.hpp>
+#include <assimp/scene.h>
 #include <assimp/postprocess.h>
 
 
 Model ModelImporter::import_from_file(const std::string& filename) {
+    DE_INFO("Importing model from file \"{0}\"", filename.c_str());
     Assimp::Importer importer;
 
     const aiScene* scene = importer.ReadFile(filename, aiProcess_CalcTangentSpace | aiProcess_Triangulate | aiProcess_JoinIdenticalVertices | aiProcess_SortByPType);
@@ -13,6 +15,8 @@ Model ModelImporter::import_from_file(const std::string& filename) {
         throw std::exception(importer.GetErrorString());
     }
 
+    DE_INFO("\tn. of meshes {0}", scene->mNumMeshes);
 
+    aiNode* node = scene->mRootNode;
     return {};
 }
