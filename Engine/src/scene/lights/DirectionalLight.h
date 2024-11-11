5ddb8f547d43f2d6ac840fe43cf26138da0a59a0
Lorenzo Lapucci <halexercraft@gmail.com>
2024-11-11 13:50:53 +0000

    Do not pass local variable as ref

--- Engine/src/scene/lights/DirectionalLight.h
+++ Engine/src/scene/lights/DirectionalLight.h
@@ -18,7 +18,7 @@ public:
         return this->rotation;
     }
 
-    const glm::mat4& getViewProjectionMatrix() const {
+    glm::mat4 getViewProjectionMatrix() const {
         const glm::mat4 projection = glm::ortho(-20.0f, 20.0f, -20.0f, 20.0f, 0.1f, 1000.0f);
         const glm::mat4 view = glm::lookAt(-this->rotation.toDirection() * 100.0f, glm::vec3(0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
         return projection * view;
