912bdffcc0431717ec2063f00b6440a14862fff4
Lorenzo Lapucci <halexercraft@gmail.com>
2025-04-12 11:27:49 +0100

    Remove test content and add callback for viewport resize

--- ui/src/views/HomeView.vue
+++ ui/src/views/HomeView.vue
@@ -2,40 +2,27 @@
 import ViewportWindow from '@/windows/ViewportWindow.vue';
 import PerformanceWindow from '@/windows/PerformanceWindow.vue';
 import Resizable from '@/components/Resizable.vue';
-import ViewportWindow2 from '@/windows/ViewportWindow2.vue';
 </script>
 
 <template>
   <div class="split-widget windows-grid">
-    <Resizable class="column" direction="horizontal" resizer-location="right" :defaultWidth="100" :minWidth="100">
+    <Resizable class="column window" direction="horizontal" resizer-location="right" :defaultWidth="200" :minWidth="100">
       <div class="column-contents">
         <PerformanceWindow />
-        <ViewportWindow2 />
-        <ViewportWindow />
       </div>
     </Resizable>
     <div class="column content-main">
       <div class="column-contents">
-        <Resizable direction="vertical" resizer-location="bottom" :defaultHeight="150" :minHeight="100" class="window">
-          <PerformanceWindow />
-        </Resizable>
+        <Resizable direction="vertical" resizer-location="bottom" :defaultHeight="150" :minHeight="100" class="window"></Resizable>
         <ViewportWindow class="content-main" />
-        <Resizable direction="vertical" resizer-location="top" :defaultHeight="150" :minHeight="100" class="window">
-          <PerformanceWindow />
-        </Resizable>
+        <Resizable direction="vertical" resizer-location="top" :defaultHeight="40" :minHeight="40" class="window"></Resizable>
       </div>
     </div>
-    <Resizable class="column" direction="horizontal" resizer-location="left" :defaultWidth="100" :minWidth="100">
+    <Resizable class="column window" direction="horizontal" resizer-location="left" :defaultWidth="200" :minWidth="100">
       <div class="column-contents">
-        <Resizable class="column" direction="vertical" resizer-location="bottom" :defaultHeight="200" :minHeight="100">
-          <PerformanceWindow />
-        </Resizable>
-        <div class="column content-main">
-          <ViewportWindow2 />
-        </div>
-        <Resizable class="column" direction="vertical" resizer-location="top" :defaultHeight="200" :minHeight="100">
-          <PerformanceWindow />
-        </Resizable>
+        <Resizable class="column" direction="vertical" resizer-location="bottom" :defaultHeight="200" :minHeight="100"></Resizable>
+        <div class="column content-main"></div>
+        <Resizable class="column" direction="vertical" resizer-location="top" :defaultHeight="200" :minHeight="100"></Resizable>
       </div>
     </Resizable>
   </div>
--- ui/src/windows/ViewportWindow.vue
+++ ui/src/windows/ViewportWindow.vue
@@ -1,5 +1,24 @@
+<script setup lang="ts">
+import { onBeforeUnmount, onMounted, ref } from 'vue';
+
+const viewport = ref<HTMLElement>();
+
+const resizeObserver = new ResizeObserver((entries) => {
+  const { width, height } = entries[0].contentRect;
+  window.call('viewportResize', Math.ceil(width), Math.ceil(height));
+});
+
+onMounted(() => {
+  resizeObserver.observe(viewport.value!);
+});
+
+onBeforeUnmount(() => {
+  resizeObserver.disconnect();
+});
+</script>
+
 <template>
-  <div>
+  <div ref="viewport">
     <!-- empty, transparent div, to allow the viewport to be visible -->
   </div>
 </template>
