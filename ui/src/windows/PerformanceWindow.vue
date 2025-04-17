c531ac2d78c4e9939639898f3f4866b73811e7c6
Lorenzo Lapucci <halexercraft@gmail.com>
2025-04-17 23:47:04 +0100

    Use ChartJS to render the frame timings

--- ui/src/windows/PerformanceWindow.vue
+++ ui/src/windows/PerformanceWindow.vue
@@ -1,16 +1,25 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue';
+import { computed, onMounted, ref, shallowRef } from 'vue';
+import { Chart, type ChartDataset } from 'chart.js/auto';
 
 const deltaTime = ref(0);
 const fps = computed(() => deltaTime.value ? (1.0 / deltaTime.value).toFixed(2) : 0);
 const timings = ref<[string, number][]>([]);
+const timingsHistory = ref<{ signature: string, timing: number }[][]>([]);
+const MAX_TIMINGS = 100;
 
 window.setMessageListener('updateFrameInfo', (dT: number) => {
   deltaTime.value = dT;
 });
 
 window.setMessageListener('updateProfilingInfo', function(newTimings: [string, number][]) {
-  timings.value = newTimings;
+  timings.value = newTimings.map(([signature, timing]) => {
+    return [signature, timing / 1000.0];
+  });
+  timingsHistory.value.push(timings.value.map(([signature, timing]) => ({ signature, timing })));
+  if (timingsHistory.value.length > MAX_TIMINGS) {
+    timingsHistory.value.shift();
+  }
 });
 
 const message = ref('false');
@@ -33,6 +42,73 @@ function formatFunctionSignature(name: string) {
   return `<span class="text-gray-200">${name}</span>`;
 }
 
+const canvas = ref<HTMLCanvasElement>();
+const chart = shallowRef<Chart | null>(null);
+
+function getDatasetsForChart() {
+  const timingsBySignature = new Map<string, number[]>();
+  timingsHistory.value.forEach(frame => {
+    for (const { signature, timing } of frame) {
+      if (!timingsBySignature.has(signature)) {
+        timingsBySignature.set(signature, []);
+      }
+      timingsBySignature.get(signature)!.push(timing);
+    }
+  });
+  const datasets: ChartDataset[] = [];
+  timingsBySignature.forEach((timings, signature) => {
+    datasets.push({
+      label: signature,
+      data: timings,
+      fill: true,
+    });
+  });
+  return datasets;
+}
+
+function updateChart() {
+  if (chart.value) {
+    chart.value.data.datasets = getDatasetsForChart();
+    chart.value.update();
+    requestAnimationFrame(updateChart);
+  }
+}
+
+onMounted(() => {
+  chart.value = new Chart(canvas.value!, {
+    type: 'line',
+    data: {
+      labels: Array.from({ length: MAX_TIMINGS }, (_, i) => i),
+      datasets: getDatasetsForChart(),
+    },
+    options: {
+      animation: false,
+      scales: {
+        x: {
+          ticks: {
+            display: false,
+          },
+          grid: {
+            display: false,
+          },
+        },
+        y: {
+          stacked: true,
+        },
+      },
+      plugins: {
+        legend: {
+          display: false,
+        },
+        tooltip: {
+          mode: 'nearest',
+          intersect: false,
+        },
+      },
+    },
+  });
+  updateChart();
+});
 </script>
 
 <template>
@@ -45,8 +121,9 @@ function formatFunctionSignature(name: string) {
     <div class="flex flex-col text-sm mt-4">
       <div v-for="[name, time] in timings" :key="name" class="flex flex-row justify-between gap-2">
         <pre v-html="formatFunctionSignature(name)"></pre>
-        <pre class="w-1/6 text-right">{{ (time / 1000.0).toFixed(2) }} ms</pre>
+        <pre class="w-1/6 text-right">{{ time.toFixed(2) }} ms</pre>
       </div>
     </div>
+    <canvas ref="canvas" class="mt-4" id="test"></canvas>
   </div>
 </template>
