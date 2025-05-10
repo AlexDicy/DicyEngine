bfba59c870bf4d53582848b19c42d2d450b7f9dc
Lorenzo Lapucci <halexercraft@gmail.com>
2025-05-10 10:39:23 +0100

    Improve input responsiveness by updating entities before drawing the frame

--- Engine/src/layers/SceneLayer.cpp
+++ Engine/src/layers/SceneLayer.cpp
@@ -136,6 +136,12 @@ SceneLayer::SceneLayer(const std::unique_ptr<Context>& ctx) {
 
 void SceneLayer::update(const std::unique_ptr<Context>& ctx) {
     DE_PROFILE_FUNCTION();
+    const auto scriptsView = this->scene->getScripts();
+    for (const auto& entity : scriptsView) {
+        Script& script = scriptsView.get<Script>(entity);
+        script.getEntityScript()->onUpdate(ctx->deltaTime);
+    }
+
     ctx->renderer->beginFrame();
 
     ctx->renderer->setDirectionalLight(this->directionalLight);
@@ -190,12 +196,6 @@ void SceneLayer::update(const std::unique_ptr<Context>& ctx) {
     ctx->renderer->drawUI(this->uiMesh->vertexArray, this->uiShader, this->uiMesh->material);
 
     ctx->renderer->endFrame();
-
-    const auto scriptsView = this->scene->getScripts();
-    for (const auto& entity : scriptsView) {
-        Script& script = scriptsView.get<Script>(entity);
-        script.getEntityScript()->onUpdate(ctx->deltaTime);
-    }
 }
 
 std::vector<Ref<Entity>> SceneLayer::addEntitiesForModels(const Ref<Renderer>& renderer, const std::string& path, const glm::vec3 position, const Rotation rotation,
