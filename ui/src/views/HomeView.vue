64c98deb86192306af20060c9d063231a82fbdcf
Lorenzo Lapucci <halexercraft@gmail.com>
2025-05-22 11:15:44 +0100

    Add key presses debugging code

--- ui/src/views/HomeView.vue
+++ ui/src/views/HomeView.vue
@@ -3,20 +3,51 @@ import ViewportWindow from '@/windows/ViewportWindow.vue';
 import PerformanceWindow from '@/windows/PerformanceWindow.vue';
 import Resizable from '@/components/Resizable.vue';
 import SceneTreeWindow from '@/windows/SceneTreeWindow.vue';
+import { ref, onMounted, onUnmounted } from 'vue';
+const ctrlPressed = ref(false);
+const shiftPressed = ref(false);
+const altPressed = ref(false);
+const metaPressed = ref(false);
+const keyPressed = ref('');
+const handleKeyDown = (event: KeyboardEvent) => {
+  ctrlPressed.value = event.ctrlKey;
+  shiftPressed.value = event.shiftKey;
+  altPressed.value = event.altKey;
+  metaPressed.value = event.metaKey;
+  keyPressed.value = event.key;
+};
+const handleKeyUp = (event: KeyboardEvent) => {
+  ctrlPressed.value = event.ctrlKey;
+  shiftPressed.value = event.shiftKey;
+  altPressed.value = event.altKey;
+  metaPressed.value = event.metaKey;
+  keyPressed.value = '';
+};
+onMounted(() => {
+  window.addEventListener('keydown', handleKeyDown);
+  window.addEventListener('keyup', handleKeyUp);
+});
+onUnmounted(() => {
+  window.removeEventListener('keydown', handleKeyDown);
+  window.removeEventListener('keyup', handleKeyUp);
+});
 </script>
 
 <template>
   <div class="split-widget windows-grid">
-    <Resizable class="column window" direction="horizontal" resizer-location="right" :defaultWidth="360" :minWidth="100">
+    <Resizable class="column window" direction="horizontal" resizer-location="right" :defaultWidth="360"
+               :minWidth="100">
       <div class="column-contents">
         <PerformanceWindow />
       </div>
     </Resizable>
     <div class="column content-main">
       <div class="column-contents">
-        <Resizable direction="vertical" resizer-location="bottom" :defaultHeight="16" :minHeight="16" class="window"></Resizable>
+        <Resizable direction="vertical" resizer-location="bottom" :defaultHeight="16" :minHeight="16"
+                   class="window"></Resizable>
         <ViewportWindow class="content-main" />
-        <Resizable direction="vertical" resizer-location="top" :defaultHeight="16" :minHeight="16" class="window"></Resizable>
+        <Resizable direction="vertical" resizer-location="top" :defaultHeight="16" :minHeight="16"
+                   class="window"></Resizable>
       </div>
     </div>
     <Resizable class="column window" direction="horizontal" resizer-location="left" :defaultWidth="200" :minWidth="100">
@@ -26,7 +57,16 @@ import SceneTreeWindow from '@/windows/SceneTreeWindow.vue';
             <SceneTreeWindow />
           </div>
         </Resizable>
-        <div class="column content-main"></div>
+        <div class="column content-main">
+          <div class="column-contents">
+            <pre>Ctrl: {{ ctrlPressed }}</pre>
+            <pre>Shift: {{ shiftPressed }}</pre>
+            <pre>Alt: {{ altPressed }}</pre>
+            <pre>Meta: {{ metaPressed }}</pre>
+            <pre>Key: {{ keyPressed }}</pre>
+            <input type="text" placeholder="type here..." />
+          </div>
+        </div>
       </div>
     </Resizable>
   </div>
